5ec5f3efaca6868f98ceb1022e146c777049a6e3
tminhbao <tminhbao2001@gmail.com>
2023-02-15 06:54:21 +0700

    add constants, setup redux and routes, templates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  unstable_HistoryRouter as HistoryRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Home from "./pages/Home/Home";
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import HomeTemplate from "./templates/HomeTemplate";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const history = createBrowserHistory();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path="" element={<HomeTemplate />}>
+          <Route path="" element={<Home />} />
+        </Route>
+        {/* <Route path="">
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route> */}
+      </Routes>
+    </HistoryRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
