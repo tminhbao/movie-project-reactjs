13294ee903beaed47a03c154828e72b3e7b30b2a
tminhbao <tminhbao2001@gmail.com>
2023-02-15 07:35:25 +0700

    add ant-design, create banner.jsx

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Carousel } from "antd";
+const Banner = () => {
+  return (
+    <Carousel autoplay>
+      <div>
+        <h3
+          style={{
+            height: "160px",
+            color: "#fff",
+            lineHeight: "160px",
+            textAlign: "center",
+            background: "#364d79",
+          }}
+        >
+          1
+        </h3>
+      </div>
+      <div>
+        <h3
+          style={{
+            height: "160px",
+            color: "#fff",
+            lineHeight: "160px",
+            textAlign: "center",
+            background: "#364d79",
+          }}
+        >
+          2
+        </h3>
+      </div>
+      <div>
+        <h3
+          style={{
+            height: "160px",
+            color: "#fff",
+            lineHeight: "160px",
+            textAlign: "center",
+            background: "#364d79",
+          }}
+        >
+          3
+        </h3>
+      </div>
+      <div>
+        <h3
+          style={{
+            height: "160px",
+            color: "#fff",
+            lineHeight: "160px",
+            textAlign: "center",
+            background: "#364d79",
+          }}
+        >
+          4
+        </h3>
+      </div>
+    </Carousel>
+  );
+};
+
+export default Banner;
