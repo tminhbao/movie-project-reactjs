f748aae71cc4e3191c2dff77eb72c143698134fb
tminhbao <tminhbao2001@gmail.com>
2023-02-16 23:26:22 +0700

    create register page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input, Typography } from "antd";
+import { Button, Checkbox, Form, Input, Typography, message } from "antd";
 import "../../assets/sass/Login/Login.scss";
 import { NavLink } from "react-router-dom";
 
@@ -8,64 +8,77 @@ const Login = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
+  const [messageApi, contextHolder] = message.useMessage();
+  const handleForgotPassword = () => {
+    messageApi.open({
+      type: "error",
+      content: "Xin lỗi, mình chưa có làm tính năng này",
+    });
+  };
   return (
-    <div
-      style={{
-        backgroundImage:
-          "url(https://demo1.cybersoft.edu.vn/static/media/backapp.b46ef3a1.jpg)",
-      }}
-      className="h-screen"
-    >
-      <Form
-        name="normal_login"
-        className="login-form bg-white w-1/4 mx-auto p-8 rounded-md relative top-1/2 -translate-y-3/4"
-        initialValues={{
-          remember: true,
+    <>
+      {contextHolder}
+      <div
+        style={{
+          backgroundImage:
+            "url(https://demo1.cybersoft.edu.vn/static/media/backapp.b46ef3a1.jpg)",
         }}
-        onFinish={onFinish}
-        size="large"
+        className="h-screen"
       >
-        <Form.Item name="username">
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item name="password">
-          <Input.Password
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            noStyle
-            className="w-1/2"
-          >
-            <Checkbox>Remember me</Checkbox>
+        <Form
+          name="normal_login"
+          className="login-form bg-white w-1/4 mx-auto p-8 rounded-md relative top-1/2 -translate-y-3/4"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          size="large"
+        >
+          <Form.Item name="username">
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Tên đăng nhập"
+            />
           </Form.Item>
-          <Typography.Text className="login-form-forgot w-1/2" href="">
-            Forgot password
-          </Typography.Text>
-        </Form.Item>
-        <Form.Item className="text-center">
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button bg-primary block w-full"
-          >
-            Log in
-          </Button>
-          <Typography.Text href="" className="mt-2 block">
-            Don't have an account?{" "}
-            <NavLink to="/register">Register now!</NavLink>
-          </Typography.Text>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item name="password">
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Mật khẩu"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              noStyle
+              className="w-1/2"
+            >
+              <Checkbox>Ghi nhớ đăng nhập</Checkbox>
+            </Form.Item>
+            <Typography.Text
+              className="login-form-forgot w-1/2 text-opacity-10 cursor-pointer"
+              onClick={handleForgotPassword}
+            >
+              Quên mật khẩu
+            </Typography.Text>
+          </Form.Item>
+          <Form.Item className="text-center">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button bg-primary block w-full"
+            >
+              Đăng nhập
+            </Button>
+            <Typography.Text href="" className="mt-2 block">
+              Chưa có tài khoản hả?{" "}
+              <NavLink to="/register">Dô đây đăng ký đi nè!</NavLink>
+            </Typography.Text>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 };
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,88 @@
+import {
+  LockOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Button, Form, Input, Typography } from "antd";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(https://demo1.cybersoft.edu.vn/static/media/backapp.b46ef3a1.jpg)",
+      }}
+      className="h-screen"
+    >
+      <Form
+        name="normal_login"
+        className="login-form bg-white w-1/4 mx-auto p-8 rounded-md relative top-1/2 -translate-y-2/3"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        size="large"
+      >
+        <Form.Item name="username">
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Tên đăng nhập"
+          />
+        </Form.Item>
+        <Form.Item name="password">
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Mật khẩu"
+          />
+        </Form.Item>
+        <Form.Item name="passwordConfirm">
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Nhập lại mật khẩu"
+          />
+        </Form.Item>
+        <Form.Item name="name">
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Họ Tên"
+          />
+        </Form.Item>
+        <Form.Item name="email">
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item name="phoneNumber">
+          <Input
+            prefix={<PhoneOutlined className="site-form-item-icon" />}
+            placeholder="Số điện thoại"
+          />
+        </Form.Item>
+        <Form.Item className="text-center">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button bg-primary block w-full"
+          >
+            Đăng ký
+          </Button>
+          <Typography.Text href="" className="mt-2 block">
+            Đã có tài khoản?{" "}
+            <NavLink to="/login">Đăng nhập ngay nào!!!</NavLink>
+          </Typography.Text>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default Register;
